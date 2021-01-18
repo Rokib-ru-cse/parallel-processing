e1a1c57bc7445dbf82db1dce5ddb8f919951d6dd
Rokib-ru-cse <rokibahmed.ru.cse@gmail.com>
2021-01-18 19:05:22 +0600

    adding hello world

--- hello_world.c
+++ hello_world.c
@@ -24,3 +24,17 @@ int main()
 
 // specifies number of thread = export OMP_NUM_THREADS=5
 // compife openmp program = gcc -fopenmmp filename
+
+/*
+What is the meaning of * (&ptr) and *(*c) in C?
+(&) -> address of operator( which gives address of opeartor)
+
+(*) -> dereference operator which gives value particularly stored in memory location
+
+let’s assume ptr is an pointer to an integer x whose memory location is 100 which store any integer let’s take 10 now ptr also has their own memory location which is 200
+
+So address of ptr (&ptr) which is 200 after dereferencing *(&ptr) -> ×(200) -> 100
+
+Now comes to the *(*c) it is nothing but a pointer to a pointer variable which hold the address of any other pointer let say c holding the address of ptr(above case) then after dereferences c (*c) it gives value at ptr which is the address of x now again dereference *(*c) which gives the gives value at x which is 10.
+
+*/
