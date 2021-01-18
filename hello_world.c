84da6e939bd2aa1c5173e085b8c4eba56bffc982
Rokib-ru-cse <rokibahmed.ru.cse@gmail.com>
2021-01-18 18:42:45 +0600

    adding hello world

--- hello_world.c
+++ hello_world.c
@@ -1,7 +1,21 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <omp.h> // including openmp header
 
-int main(){
 
 
-    return 0; 
+
+int main()
+{
+
+
+    //  # specify the block to be executed on parallel
+    #pragma opm parallel{
+
+        // print "hello world" from each thread 
+        printf("hello world");
+    }
+
+    return 0;
 }
+
+//run openmp code = (gcc -fopenmp filename)
